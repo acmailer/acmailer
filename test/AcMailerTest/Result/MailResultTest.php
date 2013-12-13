6da68f337a99fea0546690d0ef7622420fe4f593
Alejandro Celaya <alejandro@alejandrocelaya.com>
2013-12-13 16:54:59 +0100

    Added MailResult test case

---
+++ test/AcMailerTest/Result/MailResultTest.php
@@ -0,0 +1,29 @@
+<?php
+namespace AcMailerTest\Result;
+
+use AcMailer\Result\MailResult;
+
+/**
+ * 
+ * @author Alejandro Celaya Alastrué
+ * @link http://www.alejandrocelaya.com
+ */
+class MailResultTest extends \PHPUnit_Framework_TestCase
+{
+    
+    private $mailResult;
+    
+    public function testDefaultValues() {
+        $this->mailResult = new MailResult();
+        $this->assertTrue($this->mailResult->isValid());
+        $this->assertEquals("Success!!", $this->mailResult->getMessage());
+    }
+    
+    public function testCustomValues() {
+        $expectedError = "Custom error message";
+        $this->mailResult = new MailResult(false, $expectedError);
+        $this->assertFalse($this->mailResult->isValid());
+        $this->assertEquals($expectedError, $this->mailResult->getMessage());
+    }
+    
+}
