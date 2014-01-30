40eb73a86be1f8af5e471ce278913ab62147a34a
Alejandro Celaya <alejandro@alejandrocelaya.com>
2014-01-30 21:52:25 +0100

    fixes #17 Moved MailOptionsFactory to its own factory class, removing the hardcoded closure from config file. This allows config file to be serialized

--- config/module.config.php
+++ config/module.config.php
@@ -1,23 +1,17 @@
 <?php
-use Zend\ServiceManager\ServiceLocatorInterface;
-use AcMailer\Options\MailOptions;
-
 return array(
     
     'service_manager' => array(
         'factories' => array(
-            'AcMailer\Service\MailService'  => 'AcMailer\Service\Factory\MailServiceFactory',
-            'AcMailer\Options\MailOptions' => function(ServiceLocatorInterface $sm) {
-                $config = $sm->get('Config');
-                return new MailOptions(isset($config['mail_options']) ? $config['mail_options'] : array());
-            }
+            'AcMailer\Service\MailService'	=> 'AcMailer\Service\Factory\MailServiceFactory',
+            'AcMailer\Options\MailOptions' 	=> 'AcMailer\Options\Factory\MailOptionsFactory'
         ),
     ),
-    
+
     'view_manager' => array(
 		'template_path_stack' => array(
 			__DIR__ . '/../view',
 		),
     ),
-    
+
 );
---
+++ src/AcMailer/Options/Factory/MailOptionsFactory.php
@@ -0,0 +1,27 @@
+<?php
+namespace AcMailer\Options\Factory;
+
+use AcMailer\Options\MailOptions;
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+/**
+ * Class MailOptionsFactory
+ * @author Alejandro Celaya Alastrué
+ * @link http://www.alejandrocelaya.com
+ */
+class MailOptionsFactory implements FactoryInterface {
+
+	/**
+	 * Create service
+	 *
+	 * @param ServiceLocatorInterface $serviceLocator
+	 * @return mixed
+	 */
+	public function createService(ServiceLocatorInterface $serviceLocator)
+	{
+		$config = $serviceLocator->get('Config');
+		return new MailOptions(isset($config['mail_options']) ? $config['mail_options'] : array());
+	}
+
+}
