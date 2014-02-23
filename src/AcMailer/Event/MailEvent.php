aabeb16c472371f622d519dce2a1431a88fe33c8
Alejandro Celaya <alejandro@alejandrocelaya.com>
2014-02-23 21:04:56 +0100

    Created MailEvent

---
+++ src/AcMailer/Event/MailEvent.php
@@ -0,0 +1,43 @@
+<?php
+namespace AcMailer\Event;
+
+use AcMailer\Service\MailServiceInterface;
+use Zend\EventManager\Event;
+
+/**
+ * Encapsulation of a Mail event
+ * @author Alejandro Celaya Alastrué
+ * @link http://www.alejandrocelaya.com
+ */
+class MailEvent extends Event {
+
+	const EVENT_MAIL_PRE_SEND 	= 'event.mail.pre.send';
+	const EVENT_MAIL_POST_SEND 	= 'event.mail.post.send';
+	const EVENT_MAIL_SEND_ERROR	= 'event.mail.send.error';
+
+	/**
+	 * @var MailServiceInterface
+	 */
+	protected $mailService;
+
+	public function __construct(MailServiceInterface $mailService, $name = self::EVENT_MAIL_PRE_SEND) {
+		parent::__construct($name);
+		$this->mailService = $mailService;
+	}
+
+	/**
+	 * @param $mailService
+	 * @return $this
+	 */
+	public function setMailService($mailService) {
+		$this->mailService = $mailService;
+		return $this;
+	}
+	/**
+	 * @return \AcMailer\Service\MailServiceInterface
+	 */
+	public function getMailService() {
+		return $this->mailService;
+	}
+
+} 
