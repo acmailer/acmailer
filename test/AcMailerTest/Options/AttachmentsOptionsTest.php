2fd3a273c5df2c1e4dab0e5c61a8eb37e3ba1996
Alejandro Celaya <alejandro@alejandrocelaya.com>
2014-06-24 18:20:21 +0200

    Enhanced AttachmentsOptionsTest to get greater code coverage

--- test/AcMailerTest/Options/AttachmentsOptionsTest.php
+++ test/AcMailerTest/Options/AttachmentsOptionsTest.php
@@ -46,4 +46,34 @@ class AttachmentsOptionsTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(AttachmentsOptions::DEFAULT_ITERATE, $dir['iterate']);
         $this->assertEquals(true, $dir['recursive']);
     }
+
+    public function testTotalFiles()
+    {
+        $this->assertCount(0, $this->attachmentsOptions->getFiles());
+
+        $this->attachmentsOptions->setFiles(array('file1', 'file2', 'file3'));
+        $this->attachmentsOptions->addFiles(array('file4', 'file5', 'file6'));
+        $this->attachmentsOptions->addFile('file7');
+        $this->attachmentsOptions->addFile('file8');
+        $this->assertCount(8, $this->attachmentsOptions->getFiles());
+
+        $this->attachmentsOptions->setFiles(array('file1', 'file2', 'file3'));
+        $this->assertCount(3, $this->attachmentsOptions->getFiles());
+
+        $this->attachmentsOptions->addFiles(array('file4', 'file5', 'file6'));
+        $this->assertCount(6, $this->attachmentsOptions->getFiles());
+    }
+
+    public function testDirNormalization()
+    {
+        $this->attachmentsOptions->setDir(array());
+        $this->assertArrayHasKey('recursive', $this->attachmentsOptions->getDir());
+        $this->assertArrayHasKey('iterate', $this->attachmentsOptions->getDir());
+        $this->assertArrayHasKey('path', $this->attachmentsOptions->getDir());
+
+        $dir = $this->attachmentsOptions->getDir();
+        $this->assertEquals(AttachmentsOptions::DEFAULT_PATH, $dir['path']);
+        $this->assertEquals(AttachmentsOptions::DEFAULT_ITERATE, $dir['iterate']);
+        $this->assertEquals(AttachmentsOptions::DEFAULT_RECURSIVE, $dir['recursive']);
+    }
 }
