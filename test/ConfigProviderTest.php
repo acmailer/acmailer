f3db4c6dbdad6a31960c892725fac3a9af0cbcfd
Abdul Malik Ikhsan <samsonasik@gmail.com>
2016-10-09 16:22:34 +0700

    Added ConfigProvider support

---
+++ test/ConfigProviderTest.php
@@ -0,0 +1,26 @@
+<?php
+namespace AcMailerTest;
+
+use AcMailer\ConfigProvider;
+use PHPUnit_Framework_TestCase as TestCase;
+
+class ConfigProviderTest extends TestCase
+{
+    /**
+     * @var ConfigProvider
+     */
+    private $configProvider;
+
+    public function setUp()
+    {
+        $this->configProvider = new ConfigProvider();
+    }
+
+    public function testInvoke()
+    {
+        $expectedConfig = ['dependencies' => (include __DIR__ . '/../config/module.config.php')['service_manager']];
+        $returnedConfig = $this->configProvider->__invoke();
+
+        $this->assertEquals($expectedConfig, $returnedConfig);
+    }
+}
