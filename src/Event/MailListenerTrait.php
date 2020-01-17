af3825d9d9208de9b140973e2f8424c6049ab751
Alejandro Celaya <alejandro@alejandrocelaya.com>
2020-01-17 15:32:12 +0100

    Fixed return typehint removed by mistake

--- src/Event/MailListenerTrait.php
+++ src/Event/MailListenerTrait.php
@@ -11,7 +11,10 @@ trait MailListenerTrait
 {
     use ListenerAggregateTrait;
 
-    public function attach(EventManagerInterface $events, $priority = 1) // phpcs:ignore
+    /**
+     * @param int $priority
+     */
+    public function attach(EventManagerInterface $events, $priority = 1): void // phpcs:ignore
     {
         $this->listeners[] = $events->attach(MailEvent::EVENT_MAIL_PRE_RENDER, [$this, 'onPreRender'], $priority);
         $this->listeners[] = $events->attach(MailEvent::EVENT_MAIL_PRE_SEND, [$this, 'onPreSend'], $priority);
